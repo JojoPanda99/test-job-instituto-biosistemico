c733ea20b40b0436d31f79d0ece3903037725264
Joao Victor Sousa <jojo.sousa08@gmail.com>
2022-11-13 12:43:46 -0300

    create header component and create route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ListEmployersComponent} from "./employer/list-employers/list-employers.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list-employers', component: ListEmployersComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {EmployerModule} from "./employer/employer.module";
+import { HeaderComponentComponent } from './components/header-component/header-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    EmployerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
